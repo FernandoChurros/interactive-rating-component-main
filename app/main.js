3fe66e067f0f9989711288703172be1aefad0f44
Fernando Churros <fernando_churros@outlook.com>
2022-11-15 20:59:47 -0300

    add page logic

---
+++ app/main.js
@@ -0,0 +1,28 @@
+const labels = document.querySelectorAll('.checkboxs');
+const inputs = document.querySelectorAll('[data-radio]');
+const choice = document.querySelector('[data-choice]');
+const btn = document.querySelector('[data-button]');
+const wrapper = document.querySelectorAll('[data-wrapper]')
+
+let radioVal;
+
+inputs.forEach( input => {
+	input.addEventListener( 'click', (e) => {
+		radioVal = input.value;
+
+		input.labels[0].classList.add('bg-active');
+		for (let i = 0; i < labels.length; i++) {
+			if (inputs[i].checked == false) {
+				inputs[i].labels[0].classList.remove('bg-active');
+			}
+		}
+	} )
+} )
+
+
+btn.addEventListener('click', () => {
+	wrapper[0].classList.add('hide');
+
+	choice.textContent = radioVal;
+	wrapper[1].classList.remove('hide');
+})
